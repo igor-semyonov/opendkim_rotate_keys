8ca8abd71b1a180d7d8354d3cff66187f2745447
Igor Semyonov <igor@semyonov.xyz>
2025-01-18 17:44:16 -0500

    updated linode dns provider test

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,12 +1,36 @@
+import os
+import time
+
+import pytest
+
 from opendkim_rotate_keys.dns.providers import (
     LinodeDnsProvider,
 )
 
 
+@pytest.mark.skipif(
+    "LINODE_API_KEY" not in os.environ,
+    reason="API key not in environment variables",
+)
 def test_get_domains():
     ldp = LinodeDnsProvider()
     ldp.get_domains()
     print(ldp.domains)
     #  ldp.create_txt_record("nalgor.net", "meow", "mix")
-    #  records = ldp.get_records()
-    #  print(records)
+    domain = "nalgor.net"
+
+    ldp.create_txt_record(
+        domain,
+        "test",
+        "test domain key contents",
+    )
+    time.sleep(10)
+
+    records = ldp.get_records(domain)
+    for record in records["data"]:
+        if record["type"] == "TXT":
+            if record["name"] == "test_domainkey":
+                record_id = record["id"]
+                _response = ldp.delete_txt_record(
+                    domain, record_id
+                )
