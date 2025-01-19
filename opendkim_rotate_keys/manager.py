65c16dcdfe0511f4d2a6dbe9d582fb8729d95c05
Igor Semyonov <igor@semyonov.xyz>
2025-01-19 17:49:46 -0500

    moved chmod and chown of keys after insall

--- opendkim_rotate_keys/manager.py
+++ opendkim_rotate_keys/manager.py
@@ -146,17 +146,6 @@ class Manager:
                 txt_value,
             )
 
-        subprocess.run([
-            "chown",
-            f"{self.key_owner}:{self.key_group}",
-            f"{self.opendkim_keys_basedir}/*.private"
-            ])
-        subprocess.run([
-            "chmod",
-            "0640",
-            f"{self.opendkim_keys_basedir}/*.private"
-            ])
-
     def test_keys(self):
         utils.print_header("Testing keys...")
         print("")
@@ -258,6 +247,21 @@ class Manager:
         finally:
             print("")
             utils.toggle_services(False)
+
+        subprocess.run(
+            [
+                "chown",
+                f"{self.key_owner}:{self.key_group}",
+                f"{self.opendkim_keys_basedir}/*.private",
+            ]
+        )
+        subprocess.run(
+            [
+                "chmod",
+                "0640",
+                f"{self.opendkim_keys_basedir}/*.private",
+            ]
+        )
 
     def rotate_keys(self):
         if self.verbose:
